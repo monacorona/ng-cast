7b5724b47aa4d0385c3fccfb59006bc979f8558f
monacorona <monacoronababy@gmail.com>
2017-02-24 17:38:23 -0800

    Made video player, video list, and video list entry to appear

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,11 @@
 angular.module('video-player')
 
+
 .directive('app', function() {
+  
+
   return {
+    templateUrl: 'src/templates/app.html'
     // TODO
   };
 });
--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,6 +1,15 @@
 angular.module('video-player')
+
+.controller('videos', function($scope) {
+  $scope.videos = window.exampleVideoData;
+})
+
 .directive('videoList', function() {
   return {
     // TODO
+    scope: {
+      videos: '<'
+    },
+    templateUrl: 'src/templates/videoList.html'
   };
 });
