64405ec85cd666c56cea26d765e1dbe5b1c3cc76
monacorona <monacoronababy@gmail.com>
2017-02-25 17:29:50 -0800

    Passing more tests in App and videoList

--- src/components/search.js
+++ src/components/search.js
@@ -1,8 +1,20 @@
 angular.module('video-player')
 
+.controller('search', function($scope) {
+  this.result = function () {
+
+  };
+})
+
 .directive('search', function() {
   return {
     // TODO
+    scope: {
+      result: '<'
+    },
+    controller: 'search',
+    controllerAs: 'ctlr',
+    bindToController: true,
     templateUrl: 'src/templates/search.html'
   };
 });
